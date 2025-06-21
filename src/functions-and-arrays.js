116a7f215ab851b11bb51c033c7de07632dbf048
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-06-21 10:07:59 +0200

    rewrote them into arrow functions,way simpler and easier to read

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,5 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers(num1, num2) {
-  return num1 > num2 ? num1 : num2;
-}
+const maxOfTwoNumbers = (num1, num2) => num1 > num2 ? num1 : num2;
 
 // Iteration 2 | Find the Longest Word
 const words = [
@@ -14,35 +12,33 @@ const words = [
   "crackpot",
 ];
 
-function findLongestWord(wordsArray1) {
+const findLongestWord = (wordsArray1) => {
   if (wordsArray1.length === 0) return null;
-
-  return wordsArray1.reduce((longest, current) => {
-    return current.length > longest.length ? current : longest;
-  }, "");
-}
+  return wordsArray1.reduce((longest, current) => 
+    current.length > longest.length ? current : longest, ""
+  );
+};
 
 findLongestWord(words);
+
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers(numArray1) {
+const sumNumbers = (numArray1) => {
   if (numArray1.length === 0) return 0;
+  return numArray1.reduce((sum, current) => sum + current, 0);
+};
 
-  return numArray1.length > 0
-    ? numArray1.reduce((sum, current) => sum + current, 0)
-    : 0;
-}
 sumNumbers(numbers);
+
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers(numArray2) {
+const averageNumbers = (numArray2) => {
   if (numArray2.length === 0) return 0;
-
   const sum = numArray2.reduce((acc, current) => acc + current, 0);
   return sum / numArray2.length;
-}
+};
 
 // Iteration 5 | Find Elements
 const words2 = [
@@ -56,15 +52,14 @@ const words2 = [
   "disobedience",
 ];
 
-function doesWordExist(wordsArray2, word) {
+const doesWordExist = (wordsArray2, word) => {
   if (wordsArray2.length === 0) return null;
-
   return wordsArray2.reduce((exists, current) => {
     if (current === word) {
       exists = true;
     }
     return exists;
   }, false);
-}
+};
 
 doesWordExist(words2);
